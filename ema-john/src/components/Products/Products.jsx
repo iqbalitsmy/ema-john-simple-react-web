5f6768a2bb651fc5336d0047d5f76e30f4989d18
tatamax <tatamax0711@gmail.com>
2023-07-27 00:53:31 +0600

    add product

---
+++ ema-john/src/components/Products/Products.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Products.css'
+
+const Products = (props) => {
+    const { img, name, seller, quantity, price, ratings } = props.product;
+
+    return (
+        <div className='product'>
+            <img src={img} alt='' />
+            <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price:${price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Rating: {ratings} start</p>
+            </div>
+            <button className='btn-cart' type="button">Add to Cart</button>
+        </div>
+    );
+};
+
+export default Products;
