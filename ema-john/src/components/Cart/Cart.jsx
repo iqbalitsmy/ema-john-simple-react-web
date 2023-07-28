4ccf852cf6a7e8c8ad83504d719801cfa3059bb7
tatamax <tatamax0711@gmail.com>
2023-07-28 15:05:46 +0600

    calculation of product quantity

--- ema-john/src/components/Cart/Cart.jsx
+++ ema-john/src/components/Cart/Cart.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({ cart }) => {
-    console.log(cart)
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart) {
-        totalPrice += product.price;
+        // product.quantity = product.quantity || 1;
+        totalPrice += product.price * product.quantity;
         totalShipping += product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = (totalPrice*0.07);
     const grandTotal = totalPrice + totalShipping + tax;
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Shipping: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
--- ema-john/src/components/Shop/Shop.jsx
+++ ema-john/src/components/Shop/Shop.jsx
@@ -16,16 +16,34 @@ const Shop = () => {
 
     useEffect(() => {
         const storedCart = getShoppingCart();
-        console.log(products)
+        let savedCart = [];
         // Step 1: get id
         for (const id in storedCart) {
-            const saveProduct = products.find(product => product.id === id);
-            console.log(saveProduct)
+            // step 2: get product by using id
+            const addedProduct = products.find(product => product.id === id);
+            // Step 3: get quantity
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+            // console.log(addedProduct)
         }
+        setCart(savedCart);
     }, [products]);
 
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+        let newCart = [];
+        // const newCart = [...cart, product];
+        const exists = cart.find(pd => pd.id === product.id);
+        if(!exists){
+            product.quantity = 1;
+            newCart = [...cart, product]
+        } else {
+            exists.quantity +=1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
         setCart(newCart);
         addToDb(product.id);
     }
@@ -35,9 +53,9 @@ const Shop = () => {
                 {
                     products.map(product => <Products
                         key={product.id}
-                        product = {product}
-                        handleAddToCart = {handleAddToCart}
-                        >
+                        product={product}
+                        handleAddToCart={handleAddToCart}
+                    >
                     </Products>)
                 }
             </div>
