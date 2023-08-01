57441340f3faaf4f081b538f5e884d1084045ba7
tatamax <tatamax0711@gmail.com>
2023-08-01 22:48:01 +0600

    Add react router and update shoping order

---
+++ ema-john/src/components/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ReviewItem.css'
+import SvgReturn from '../../SvgReturn';
+
+const ReviewItem = ({ product, handleRemoveCart}) => {
+    const { id, img, price, name, quantity } = product;
+    return (
+        <div className='review-item'>
+            <img src={img} alt="" />
+            <div className='review-details'>
+                <p className='product-title'>{name}</p>
+                <p>Price: <span className='orange-text'>${price}</span></p>
+                <p>Order Quantity: <span className='orange-text'>{quantity}</span></p>
+            </div>
+            <button className='btn-delete'
+            onClick={() => handleRemoveCart(id)}
+            ><SvgReturn></SvgReturn></button>
+        </div>
+    );
+};
+
+export default ReviewItem;
