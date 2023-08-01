f355b55bdebb74cd5a02bc20f51ee5d39a5df475
tatamax <tatamax0711@gmail.com>
2023-08-01 23:10:43 +0600

    add clear cart

--- ema-john/src/components/Cart/Cart.jsx
+++ ema-john/src/components/Cart/Cart.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './Cart.css'
+import SvgReturn from '../../SvgReturn';
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleClearCart }) => {
     let totalPrice = 0;
     let totalShipping = 0;
     let quantity = 0;
-    for(const product of cart) {
+    for (const product of cart) {
         // product.quantity = product.quantity || 1;
         totalPrice += product.price * product.quantity;
         totalShipping += product.shipping;
         quantity = quantity + product.quantity;
     }
-    const tax = (totalPrice*0.07);
+    const tax = (totalPrice * 0.07);
     const grandTotal = totalPrice + totalShipping + tax;
     return (
         <div className='cart'>
@@ -21,6 +22,10 @@ const Cart = ({ cart }) => {
             <p>Shipping: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
             <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
+            <button onClick={handleClearCart} type="button" className='btn-clear-cart'>
+                <span>Clear Cart</span>
+                <SvgReturn></SvgReturn>
+            </button>
         </div>
     );
 };
--- ema-john/src/components/Shop/Shop.jsx
+++ ema-john/src/components/Shop/Shop.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css'
 import Products from '../Products/Products';
 import Cart from '../Cart/Cart';
-import { addToDb, getShoppingCart } from '../../utilities/fakedb';
+import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fakedb';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -36,17 +36,23 @@ const Shop = () => {
         let newCart = [];
         // const newCart = [...cart, product];
         const exists = cart.find(pd => pd.id === product.id);
-        if(!exists){
+        if (!exists) {
             product.quantity = 1;
             newCart = [...cart, product]
         } else {
-            exists.quantity +=1;
+            exists.quantity += 1;
             const remaining = cart.filter(pd => pd.id !== product.id);
             newCart = [...remaining, exists];
         }
         setCart(newCart);
         addToDb(product.id);
     }
+
+    const handleClearCart = () => {
+        setCart([]);
+        deleteShoppingCart();
+    }
+
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -60,7 +66,10 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <Cart cart={cart}></Cart>
+                <Cart
+                    cart={cart}
+                    handleClearCart={handleClearCart}
+                ></Cart>
             </div>
         </div>
     );
